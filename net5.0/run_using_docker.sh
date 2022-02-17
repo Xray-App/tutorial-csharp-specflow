fe58d36e07fca7904b57cc0c30b159a7fdf6e752
Sergio Freire <sergio.freire@xpand-it.com>
2022-02-17 15:57:10 +0000

    fix Dockerfile; added preliminary workflows for GH build

--- net5.0/run_using_docker.sh
+++ net5.0/run_using_docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 docker build . -t specflow_specrun_net5_tests
-docker run --rm -v $(pwd)/TestResults:/source/SpecFlowCalculator.Specs/TestResults -t specflow_specrun_net5_tests
+docker run --rm -v $(pwd)/TestResults:/source/Calculator.Specs/TestResults -t specflow_specrun_net5_tests
